6db3ee8215e340109edf4f1c5e4ef4181a74a2e4
Licar <84367661+Licarsmeth@users.noreply.github.com>
2024-05-02 08:20:17 +0545

    add shop button

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,11 +3,22 @@ import heroine from "../assets/heroine.avif"
 const Hero = function () {
     return (
         <>
-        <div className="description"></div>
+        <div className="herotext">
+            <div className="heading">
+                Welcome to Store!
+            </div>
+            <div className="subheading">
+                (A very unique name)
+            </div>
+            <div className="description">
+                Home of Exceptional quality fake products.<br /> I assure you, none of these are real.
+            </div>
+        </div>
         <div className="heroimg">
             <img src={hero} alt="hero" />
             <img src={heroine} alt="heroine" />
         </div>
+        <button className="shop-button">Shop Now!</button>
         </>
     )
 }
