ab88508a58064b1b6f09dae6262029b219bc5b81
Licar <84367661+Licarsmeth@users.noreply.github.com>
2024-05-02 14:57:51 +0545

    products added

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,7 +1,45 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import Navbar from "./Navbar";
+
 const Products = function () {
-    return(
-        <div>hello</div>
-    )
-}
+  const [products, setProducts] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async function () {
+      try {
+        const response = await fetch("https://fakestoreapi.com/products", {
+          mode: "cors",
+        });
+        const data = await response.json();
+        setProducts(data);
+        console.log(data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchData();
+  }, []);
+  return (
+    <>
+    <Navbar />
+    <div className="product">
+      {products?(
+      products.map((product) => (
+        <div key={product.id}>
+            <h3>{product.title}</h3>
+            <p>{product.description}</p>
+            <img src={product.image} alt="" />
+            <p>{product.price}</p>
+        </div>
+      ))
+    ): 
+    <p>Loading</p>
+    }
+    </div>
+    </>
+  );
+};
 
 export default Products;
