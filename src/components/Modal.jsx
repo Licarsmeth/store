f9b42843ce3c043eb69f58d3a167ebde6aa2bc5e
Licar <84367661+Licarsmeth@users.noreply.github.com>
2024-05-06 08:01:36 +0545

    add cart input, remove dialog box bug

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,27 +1,36 @@
 /* eslint-disable react/prop-types */
 const Modal = ({ product, onClose }) => {
-    const handleClose = () => {
-      onClose();
-    };
-  
-    return (
-      <>
-        {product && (
-          <dialog open>
-            <div className="modal-overlay">
-                <div className="modal">
-                  <button className="close-button" onClick={handleClose}>×</button>
-                  <h2>{product.title}</h2>
-                  <img src={product.image} alt={product.title} className="productimg" />
-                  <p className="productprice">${product.price}</p>
-                  <p className="modal-description">{product.description}</p>
-                  <button className="cart-button">Add to Cart</button>
-                </div>
-            </div>
-          </dialog>
-        )}
-      </>
-    );
+  const handleClose = () => {
+    onClose();
   };
 
-  export default Modal;
+  return (
+    <>
+      {product && (
+        <dialog open>
+          <div className="modal-overlay">
+            <div className="modal">
+              <button className="close-button" onClick={handleClose}>
+                ×
+              </button>
+              <h2>{product.title}</h2>
+              <img
+                src={product.image}
+                alt={product.title}
+                className="productimg"
+              />
+              <p className="productprice">${product.price}</p>
+              <p className="modal-description">{product.description}</p>
+              <div className="cart-row">
+                <input type="number" className="cart-input" />
+                <button className="cart-button">Add to Cart</button>
+              </div>
+            </div>
+          </div>
+        </dialog>
+      )}
+    </>
+  );
+};
+
+export default Modal;
